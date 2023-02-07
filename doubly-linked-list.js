94d1d4709520b21eee341a0f85a22a7f43ca6217
Diane <hello@dianenguyen.ca>
2023-02-06 16:33:12 -0800

    starting doubly linked

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -1,7 +1,13 @@
 /** Node: node for a doubly linked list. */
 
 class Node {
-  
+  val = null;
+  next = null;
+  prev = null;
+
+  constructor(val) {
+    this.val = val;
+  }
 }
 
 class DoublyLinkedList {
@@ -16,19 +22,42 @@ class DoublyLinkedList {
   /** get(idx) returns a node at the given index */
 
   _get(idx) {
-    
+    if (idx > this.length - 1 || idx < 0) throw new Error("invalid index!");
+    let currentNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode
   }
 
   /** push(val): add new value to end of list. */
 
   push(val) {
-    
+    let newNode = new Node(val);
+
+    if (this.head === null) this.head = newNode;
+    if (this.tail !== null) this.tail.next = newNode;
+
+    newNode.prev = this.tail;
+    this.length++;
+    this.tail = newNode;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-    
+    let newNode = new Node(val);
+
+    if (this.head !== null) {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+    }
+    if (this.tail === null) this.tail = newNode;
+    this.length++;
+
+    this.head = newNode;
   }
 
   /** pop(): remove last item & return its value */
@@ -40,37 +69,38 @@ class DoublyLinkedList {
   /** shift(): remove first item & return its value */
 
   shift() {
-    
+
   }
 
   /** getAt(idx): get val at idx.*/
 
   getAt(idx) {
-   
+    return this.getByIdx(idx).val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-   
+    let currentNode = this.getByIdx(idx);
+    currentNode.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-   
+
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-   
+
   }
 
   /** return average (mean) of list values. */
 
   average() {
-   
+
   }
 }
 
